494abe6125da75c9ea4a1ddd168a8e755013ee21
Arthur Vos <arthur@mandelo.nl>
2024-01-15 14:23:27 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+// 'use strict'
+
+// import './style.css'
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
+// import { init as flipbook } from 'flipbook-viewer';
+// import * as book from './book-pdfs.js'
+// // import {DeskundigheidsverklaringBeoordelaar} from './DeskundigheidsverklaringBeoordelaar.pdf'
+
+
+// // let book = 'public/DeskundigheidsverklaringBeoordelaar.pdf';
+
+// flipbook(book, 'flipbook', (err, viewer) => {
+//   if(err) console.error(err);
+
+//   console.log('Number of pages: ' + viewer.page_count);
+//   viewer.on('seen', n => console.log('page number: ' + n));
+
+//   next.onclick = () => viewer.flip_forward();
+//   prev.onclick = () => viewer.flip_back();
+//   zoom.onclick = () => viewer.zoom();
+
+// });
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+    
+//   </div>
+// `
+
+// setupCounter(document.querySelector('#counter'))
